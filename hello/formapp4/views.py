b6cfe3be2e944e98d86a5a54f1a8b58692a66ff1
jgrynczewski <Tel^)%322))$>
2021-07-04 14:21:08 +0200

    orm

--- hello/formapp4/views.py
+++ hello/formapp4/views.py
@@ -1,18 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
+
+from formapp4.models import Task
 
 
 def register(request):
-    return render(
-        request,
-        'formapp4/register.html'
-    )
+    if request.method == "GET":
+        return render(
+            request,
+            'formapp4/register.html'
+        )
+
+    elif request.method == "POST":
+        task = request.POST.get('task')
+        if task:
+            task = Task(text=task)
+            task.save()
+
+        return redirect('formapp4:tasks_list')
+        # return HttpResponseRedirect(reverse('formapp4:tasks_list'))
 
 
 def tasks_list(request):
+    tasks = Task.objects.all()
+
     return render(
         request,
         'formapp4/tasks-list.html',
-        context = {
-            "tasks": []
+        context={
+            "tasks": tasks
         }
     )
